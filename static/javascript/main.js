3bafeefc6438128fd6f65c7d1d30a6c19a4e364c
albertcarrillosorolla <albertcarrillosorolla@MacBook-Pro-de-Albert.local>
2016-03-14 22:28:24 +0100

    fades i logo
    Fades del contingut i animació del logo en el menu

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -17,7 +17,8 @@ function navigateToPage() {
    $.get(pageName, function (response) {
         var markup = $("<div>" + response + "</div>");
       	var fragment = markup.find("#contentloader").html();
-       $("#contentloader").html(fragment);
+				$("#contentloader").fadeOut('fast',function(){$("#contentloader").html(fragment).fadeIn();});
+
    });
 	 //alert(pageName);
 	 if(pageName!=""){
@@ -72,67 +73,15 @@ $("#menu-logo").click(function(e){
 function goDown(){
 	$("body").css("overflow", "auto");
 	CONTENT_STATE = true;
-	$("html, body").animate({ scrollTop: $(".menu-div").offset().top }, 1000);
-	$("#menu-logo").animate({width:'50px'},800, function(){
-		$("#menu-logo").fadeIn().css("display","inline-block");
-	});
+	$("html, body").animate({ scrollTop: $(".menu-div").offset().top }, 1000, function(){
+	$(".menu-div ul").animate({ 'margin-left': "0" }, 400, function(){
+	$("#menu-logo").animate({ opacity: 1.0 });});});
 }
 function goUp(){
 	$("body").css("overflow", "hidden");
 	CONTENT_STATE = false;
-	$("#menu-logo").fadeOut(800, function(){$("#menu-logo").animate({width:'0px'},1000)});
-	$("html, body").animate({ scrollTop: 0 }, 1000);
+	$("html, body").animate({ scrollTop: 0 }, 1000, function(){
+	$("#menu-logo").animate({ opacity: 0 }, 400, function(){
+	$(".menu-div ul").animate({ 'margin-left': "-80px" });});});
+	$("#contentloader").fadeOut();
 }
-
-/*
-
-
-var CONTENT_STATE = false;
-$("#tradiciolink").click(function(e){
-	$("#contentloader").fadeOut("fast", function(){$("#contentloader").load("tradicioqualitat", function(response, status, xhr) {$(this).fadeIn();});});
-	goDown();
-});
-$("#creacionslink").click(function(e){
-	$("#contentloader").fadeOut("fast", function(){$("#contentloader").load("lesnostrescreacions", function(response, status, xhr) {$(this).fadeIn();});});
-	goDown();
-});
-$("#establimentslink").click(function(e){
-	$("#contentloader").fadeOut("fast", function(){$("#contentloader").load("establiments", function(response, status, xhr) {$(this).fadeIn();});});
-	goDown();
-});
-$("#noticieslink").click(function(e){
-	$("#contentloader").fadeOut("fast", function(){$("#contentloader").load("noticies", function(response, status, xhr) {$(this).fadeIn();});});
-	goDown();
-});
-$("#contactalink").click(function(e){
-	$("#contentloader").fadeOut("fast", function(){$("#contentloader").load("contacta", function(response, status, xhr) {$(this).fadeIn();});});
-	goDown();
-});
-var lastScrollTop = 0;
-$(window).on('scroll', function() {
-    st = $(this).scrollTop();
-    if(CONTENT_STATE && st < lastScrollTop) {
-        goUp();
-    }
-    lastScrollTop = st;
-    return;
-});
-
-$("#menu-logo").click(function(e){
-	goUp();
-});
-
-function goDown(){
-	$("body").css("overflow", "auto");
-	CONTENT_STATE = true;
-	$("html, body").animate({ scrollTop: $(".menu-div").offset().top }, 1000);
-	$("#menu-logo").animate({width:'50px'},800, function(){
-		$("#menu-logo").fadeIn().css("display","inline-block");
-	});
-}
-function goUp(){
-	$("body").css("overflow", "hidden");
-	CONTENT_STATE = false;
-	$("#menu-logo").fadeOut(800, function(){$("#menu-logo").animate({width:'0px'},1000)});
-	$("html, body").animate({ scrollTop: 0 }, 1000);
-}*/
