32f0d1610fc010a5112efcc29b76934de331b956
albertcarrillosorolla <albertcarrillosorolla@MacBook-Pro-de-Albert.local>
2016-03-14 20:12:59 +0100

    Nova navegació
    Nova navegació que inclou historial i accés directe a les pàgines

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,3 +1,49 @@
+$(function () {
+
+function getPageName() {
+    var pathName = window.location.pathname;
+    var pageName = "";
+    if (pathName.indexOf("/") != -1) {
+        pageName = pathName.split("/").pop();
+    } else {
+        pageName = pathName;
+    }
+    return pageName;
+}
+
+function navigateToPage() {
+
+   var pageName = getPageName();
+   $.get(pageName, function (response) {
+        var markup = $("<div>" + response + "</div>");
+      	var fragment = markup.find("#contentloader").html();
+       $("#contentloader").html(fragment);
+   });
+}
+
+$("a[data-role='ajax']").click(function (e) {
+    if (Modernizr.history) {
+        e.preventDefault();
+        var pageName = $(this).attr("href");
+        window.history.pushState(null, "", pageName);
+        navigateToPage();
+    }
+});
+
+var _popStateEventCount = 0;
+$(window).on('popstate', function (e) {
+    this._popStateEventCount++;
+    //if ($.browser.webkit && this._popStateEventCount == 1) {
+    //    return;
+    //}
+    navigateToPage();
+});
+});
+
+
+
+/*
+
 
 var CONTENT_STATE = false;
 $("#tradiciolink").click(function(e){
@@ -39,7 +85,7 @@ function goDown(){
 	CONTENT_STATE = true;
 	$("html, body").animate({ scrollTop: $(".menu-div").offset().top }, 1000);
 	$("#menu-logo").animate({width:'50px'},800, function(){
-		$("#menu-logo").fadeIn().css("display","inline-block");	
+		$("#menu-logo").fadeIn().css("display","inline-block");
 	});
 }
 function goUp(){
@@ -47,4 +93,4 @@ function goUp(){
 	CONTENT_STATE = false;
 	$("#menu-logo").fadeOut(800, function(){$("#menu-logo").animate({width:'0px'},1000)});
 	$("html, body").animate({ scrollTop: 0 }, 1000);
-}
+}*/
